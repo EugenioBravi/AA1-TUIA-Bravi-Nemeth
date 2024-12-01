37bfbf9ff789e8093ee4f25c292fcdaab54aec75
EugenioBravi <bravieugenio@gmail.com>
2024-11-30 21:08:30 -0300

    Agrego modelo a docker

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # Docker Setup
 
 ## Ciudades utilizadas
@@ -17,32 +16,36 @@
 ## Pasos para usar Docker
 
 1. **Ubicar la terminal en la carpeta "docker":**
-   
+
    En Windows:
+
    ```bash
-   cd C:\Users\Usuario\repos\AA1-TUIA-Bravi-Nemeth\docker
+   cd \AA1-TUIA-Bravi-Nemeth\docker
    ```
 
 2. **Crear la imagen Docker:**
    Ejecuta el siguiente comando para crear la imagen `infer-image`:
+
    ```bash
    docker build -t infer-image .
    ```
 
 3. **Subir el archivo `.csv` a predecir:**
-   
+
    Coloca el archivo `weather_docker.csv` en la carpeta `docker`.
 
 4. **Correr el contenedor:**
 
    En Linux:
+
    ```bash
-   docker run --rm -v $(pwd):/output infer-image
+   docker run --rm -v $(pwd):/ infer-image
    ```
 
    En Windows:
+
    ```bash
-   docker run --rm -v ${PWD}:/output infer-image
+   docker run --rm -v ${PWD}:/ infer-image
    ```
 
    Esto generará el archivo `predicciones.csv` en la misma carpeta.
--- docker/inferencia.py
+++ docker/inferencia.py
@@ -1,28 +1,88 @@
 import pandas as pd
 import numpy as np
-import joblib
-import sys
-
+import keras
 def infer(data_path):
-    # Si se esta usando el weather_docker.csv que viene con el repo, las predicciones deberian ser:
-    #No
-    #Yes
-    #No
-    #Yes
-    #Yes
+
+
     try:
+        # Leemos el modelo
+        modelo_nn = keras.models.load_model("./models/modelo_nn.h5")
+        # Leemos el archivo a predecir
         data = pd.read_csv(data_path)
-        print(f"Datos cargados desde {data_path}")
-        print(f"Columnas: {data.columns}")
+        medias_mensuales = pd.read_csv('./models/medias_mensuales.csv')
+        modas_mensuales = pd.read_csv('./models/modas_mensuales.csv')
 
-        # make a new result dataframe to return a predictions.csv as output
+        # Tratamos las fechas
+        data['Date'] = pd.to_datetime(data['Date'])
+        data['Month'] = data['Date'].dt.month
+        data.drop('Date', axis=1, inplace=True)
+
+        # Tratamos valores faltantes
+        for index, row in data.iterrows():
+            month = row['Month'] - 1  
+            for column in medias_mensuales.columns:
+                if pd.isna(data.loc[index, column]):  
+                    data.loc[index, column] = medias_mensuales.loc[month, column]
+
+        for index, row in data.iterrows():
+            month = row['Month'] - 1  
+            for column in modas_mensuales.columns:
+                if pd.isna(data.loc[index, column]): 
+                    data.loc[index, column] = modas_mensuales.loc[month, column]
+        
+        # Tratamos RainToday
+        data['RainToday'] = data['RainToday'].map({'Yes': 1, 'No': 0})
+
+        #Tratamos las ciudades
+        location_coordenadas = {
+            'Wollongong': [-34.43305,150.88305 ],
+            'Ballarat': [-37.56622, 143.84957],
+            'MountGinini': [-35.28346, 149.12807],
+            'Hobart': [-42.88333,147.31666 ],
+            'Williamtown': [-32.80899, 151.83899],
+            'Adelaide': [-34.92866, 138.59863],
+            'Sale': [-33.86785, 151.20732],
+            'Penrith': [-33.75111, 150.69416],
+            'Woomera': [-31.16563, 136.81926],
+            'SalmonGums': [-32.98207, 121.64416]
+        }
+        data['Latitud'] = data['Location'].map(lambda loc: location_coordenadas[loc][0])
+        data['Longitud'] = data['Location'].map(lambda loc: location_coordenadas[loc][1])
+        data = data.drop(columns=['Location'])
+
+        #Tratamos los puntos cardinales
+        angulos = {
+            'N': 0,
+            'NNE': 22.5,
+            'NE': 45,
+            'ENE': 67.5,
+            'E': 90,
+            'ESE': 112.5,
+            'SE': 135,
+            'SSE': 157.5,
+            'S': 180,
+            'SSW': 202.5,
+            'SW': 225,
+            'WSW': 247.5,
+            'W': 270,
+            'WNW': 292.5,
+            'NW': 315,
+            'NNW': 337.5
+        }
+        for column in ['WindGustDir','WindDir9am','WindDir3pm']:
+            data[column] = data[column].map(angulos)
+
+        #Hacemos la prediccion
+        y_pred_nn = modelo_nn.predict(data)
+        y_pred_nn = np.where(y_pred_nn >= 0.5, 1, 0)
         result = pd.DataFrame()
-        result["ID"] = data["MaxTemp"]
+        result['llueve'] = y_pred_nn
+        result['llueve'] = result['llueve'].map({1:'Yes'  , 0: 'No'})
 
-        result.to_csv("/output/predicciones.csv", index=False)
+        result.to_csv("./predicciones.csv", index=False)
         print("Predicciones guardadas en predicciones.csv")
     except Exception as e:
         print(f"Error: {e}")
 
 if __name__ == "__main__":
-    infer("/output/weather_docker.csv")
+    infer("./weather_docker.csv")
